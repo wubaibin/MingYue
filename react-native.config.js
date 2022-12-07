d10c5fd3d0547551c577dfb67cee0952a0ed7b9b
wubaibin <2448428125@qq.com>
2022-12-07 15:32:40 +0800

    feat 新增Icon

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Version: 1.0.0
+ * @Autor: wubaibin
+ * @Date: 2022-12-07 14:39:46
+ * @LastEditors: wubaibin
+ * @LastEditTime: 2022-12-07 14:40:08
+ */
+module.exports = {
+  assets: ['./src/assets/fonts/']
+};
